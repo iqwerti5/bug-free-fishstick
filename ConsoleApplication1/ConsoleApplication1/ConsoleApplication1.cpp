8789edeb5c2904aa756a11bfac80e3755cd5e5a4
iqwerti5 <dan4524o55@gmail.com>
2025-10-29 21:14:28 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	
+	short num;
+	
+	std::cin >> num;
+	
+	switch (num) 
+	{
+	case 5: std::cout << "Вы прошли проверку!";
+		break;
+		
+		switch (num)
+		{
+		case 1: std::cout << "Вы не прошли проверку!";
+			break;
+		}
+		
+		switch (num)
+		{
+		case 2: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 4: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 6: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 7: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 8: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 9: std::cout << "вы не прошли проверку!";
+			break;
+		}
+		switch (num)
+		{
+		case 10: std::cout << "вы не прошли проверку!";
+			break;
+		}
+	}
+	return 0;
+}
